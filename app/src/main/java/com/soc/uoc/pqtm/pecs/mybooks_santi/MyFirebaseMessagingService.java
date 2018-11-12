51845eeb1558d35fd669f5d36cb1572fa2d4996f
Santi <smassague@uoc.edu>
2018-11-12 12:35:03 +0100

    Pla3

---
+++ app/src/main/java/com/soc/uoc/pqtm/pecs/mybooks_santi/MyFirebaseMessagingService.java
@@ -0,0 +1,55 @@
+package com.soc.uoc.pqtm.pecs.mybooks_santi;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG= "MyFirebaseMsgErvice";
+    private static final String NOTIFICATION_CHANNEL_ID ="channel_id_01"; ;
+
+    /**
+     * Método llamado cuando se recibe un mensaje remoto
+     *
+     * @param remoteMessage Mensaje recibido de Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Mostrar una notificación al recibir un mensaje de Firebase
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    /**
+     * Crea y muestra una notificación al recibir un mensaje de Firebase
+     *
+     * @param messageBody Texto a mostrar en la notificación
+     */
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, BookListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Ejemplo Firebase")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+}
